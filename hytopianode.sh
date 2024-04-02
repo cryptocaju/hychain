4b4f2a69af914cfabe68f549ca1679c6f3bf5e24
cryptocaju <49980519+cryptocaju@users.noreply.github.com>
2024-04-02 02:37:29 +0100

    Update hytopianode.sh

--- hytopianode.sh
+++ hytopianode.sh
@@ -16,17 +16,17 @@ sleep 8s
 echo  -e  "\e[1;33m===============================INICIANDO EM 5 SEGUNDOS============================================================\e[0m"
 sleep 5s
 sudo apt update && upgrade -y
-echo  -e  "\e[1;33m================================INSTALANDO DOCKER===============================================================\e[0m"
+echo  -e  "\e[1;33m================================INSTALANDO PACOTES===============================================================\e[0m"
 sleep 7s
 echo  -e  "\e[1;33m================================INSTALACAO COM SUCESSO==========================================================\e[0m"
 sudo apt-get install curl -y
-curl -L -o https://github.com/HYCHAIN/guardian-node-software/blob/main/cli/linux/guardian-cli-linux
+sudo apt install unzip
+curl -L https://github.com/HYCHAIN/guardian-node-software/blob/main/cli/linux/guardian-cli-linux -o guardian-clo-linux.zip
 unzip guardian-cli-linux.zip
 echo -n "Enter with your private key: "
-read private_key
+read -s private_key
 echo  -e  "\e[1;34m================================RODANDO O NODE=========================================\e[0m"
 ./guardian-cli-linux guardian run "$private_key" --loop-interval-ms 3600000
 sleep 5s
-echo  -e  "\e[1;33m============================INSTALACAO COMPLETA, VAMOS AO PROXIMO PASSO :)=======================================\e[0m"
-
+echo  -e  "\e[1;33m============================INSTALACAO COMPLETA=======================================\e[0m"
 
